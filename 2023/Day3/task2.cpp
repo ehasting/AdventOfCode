a67f7c4d85e38c61c03ddf6bfe4b2724aedeabe6
Egil Hasting <eh@synergysky.com>
2023-12-08 10:02:20 +0100

    working day3_2

--- 2023/Day3/task2.cpp
+++ 2023/Day3/task2.cpp
@@ -38,21 +38,18 @@ void task2::Run()
 
     auto iter_end = std::sregex_iterator ();
     std::vector<int> digits_list;
-    for (int x = 0; x < lines.size();)
+    for (int x = 0; x < lines.size();x++)
     {
         auto &l = lines[x];
-        for (int innerx = 0; innerx < l.size(); innerx++ )
+        for (int innerx = 0; innerx < l.size(); ++innerx )
         {
             std::string digit = std::string(1, l[innerx] );
             if (l[innerx] != '*')
             {
-                std::cout << innerx  << " of "  << l.size() << " " << digit << std::endl;
+
                 continue;
             }
             std::string chksum;
-
-            std::cout << digit << std::endl;
-
             auto startpos = innerx;
             auto length = digit.size();
 
@@ -69,7 +66,7 @@ void task2::Run()
             if (isattop)
             {
                 chksum.append(lines[x].substr(spos,len));
-                chksum.append( lines[x+1].substr(spos,len));
+                chksum.append(lines[x+1].substr(spos,len));
             }
             else if (isatbottom)
             {
@@ -82,49 +79,161 @@ void task2::Run()
                 chksum.append(lines[x].substr(spos,len));
                 chksum.append(lines[x+1].substr(spos,len));
             }
-            //std::cout << std::endl;
-
-            if ( std::regex_search(chksum, std::regex(R"(\d)")) )
+            int matchcount = 0;
+            bool found = false;
+            std::smatch digit_match;
+            std::string current_checksum = chksum;
+            while ( std::regex_search(current_checksum, digit_match, std::regex(R"(\d+)")) )
             {
-                int d = std::stoi(digit);
-                digits_list.emplace_back(d);
+                found = true;
+                current_checksum = digit_match.suffix().str();
+            }
+
+            if (found)
+            {
+                std::vector<int> rval;
                 if (isattop)
                 {
-                    std::cout << lines[x].substr(spos,len)      << std::endl;
-                    std::cout << lines[x+1].substr(spos,len)    << std::endl;
+                    {
+                        auto t = this->FindFullNumber(lines[x].substr(spos, len), lines[x], spos);
+                        rval.insert(rval.end(), t.begin(), t.end());
+                    }
+                    {
+                        auto t = this->FindFullNumber(lines[x + 1].substr(spos, len), lines[x + 1], spos);
+                        rval.insert(rval.end(), t.begin(), t.end());
+                    }
+
+
                 }
                 else if (isatbottom)
                 {
-                    std::cout << lines[x-1].substr(spos,len)    << std::endl;
-                    std::cout << lines[x].substr(spos,len)      << std::endl;
+                    {
+                        auto t = this->FindFullNumber(lines[x - 1].substr(spos, len), lines[x - 1], spos);
+                        rval.insert(rval.end(), t.begin(), t.end());
+                    }
+                    {
+                        auto t = this->FindFullNumber(lines[x].substr(spos, len), lines[x], spos);
+                        rval.insert(rval.end(), t.begin(), t.end());
+                    }
                 }
                 else
                 {
-                    std::cout << lines[x-1].substr(spos,len)    << std::endl;
-                    std::cout << lines[x].substr(spos,len)      << std::endl;
-                    std::cout << lines[x+1].substr(spos,len)    << std::endl;
-                }
-                /*
-                std::cout << " " << "FOUND ("
-                    << chksum << ") Cleaned: "
-                    << d << " | sum = "
-                    << sum << " + "
-                    << d
-                    << " ( = " << sum+d << ")"
-                    <<  std::endl;
-                */
-                sum = sum + d;
-            }
-            else
-            {
-                std::cout << " " << "NOT FOUND (" << chksum << ")" <<std::endl;
-                notfoundcount++;
-            }
+                    {
+                        auto t = this->FindFullNumber(lines[x - 1].substr(spos, len), lines[x - 1], spos);
+                        rval.insert(rval.end(), t.begin(), t.end());
+                    }
+                    {
+                        auto t = this->FindFullNumber(lines[x].substr(spos, len), lines[x], spos);
+                        rval.insert(rval.end(), t.begin(), t.end());
+                    }
+                    {
+                        auto t = this->FindFullNumber(lines[x + 1].substr(spos, len), lines[x + 1], spos);
+                        rval.insert(rval.end(), t.begin(), t.end());
+                    }
 
+
+                }
+                int factor = 1;
+                std::cout << std::endl;
+                if (rval.size() == 2)
+                {
+
+
+                for(auto a:rval)
+                {
+                    std::cout << factor << " * " << a << std::endl;
+                    factor = factor * a;
+                }
+                sum = sum + factor;
+                }
+                std::cout << std::endl;
+            }
         }
 
     }
 
     std::cout << "Not found: " << notfoundcount << std::endl;
     std::cout << "Task1 SUM: " << sum <<std::endl;
+}
+std::vector<int> task2::FindFullNumber(std::string cutted, std::string &s, int sp)
+{
+    std::vector<int> rval;
+    std::string search = cutted;
+    std::smatch founddigits;
+    std::vector<task2::search> positions;
+    auto digitsregex = std::regex("\\d+");
+    auto digits_begin = std::sregex_iterator(cutted.begin(), cutted.end(), digitsregex);
+    auto iter_end = std::sregex_iterator();
+    for (auto n = digits_begin; n != iter_end; ++n)
+    {
+        std::smatch match = *n;
+        task2::search rval;
+        rval.pos = sp + match.position();
+        if (match.position() == 0)
+        {
+            rval.atstart = true;
+        }
+        if (match.position() + match.str().length() == cutted.length())
+        {
+            rval.atend = true;
+        }
+        positions.push_back(rval);
+
+    }
+
+    if (positions.size() == 0)
+    {
+        return rval;
+    }
+
+    for(auto pos: positions)
+    {
+        int startposition = pos.pos;
+
+        bool reverse_search = pos.atstart;
+
+        if (reverse_search)
+        {
+            for (int x = startposition;;)
+            {
+                if (std::isdigit(s[x]))
+                {
+                    if (x == 0)
+                    {
+                        startposition = 0;
+                        break;
+                    }
+                    x--;
+                    continue;
+                }
+                startposition = x;
+                break;
+            }
+        }
+
+        std::string outstring;
+
+        bool innumber = false;
+        for (int x = startposition;;)
+        {
+            if (std::isdigit(s[x]))
+            {
+                innumber = true;
+                outstring.append(std::string(1, s[x]));
+
+            }
+            if (!std::isdigit(s[x]) && innumber)
+            {
+                break;
+            }
+            x++;
+        }
+        rval.push_back(std::stoi(outstring));
+    }
+    for(auto n:rval)
+    {
+        std::cout << "RVAL: " << n << std::endl;
+    }
+    return rval;
+
 }
--- 2023/Day3/task2.hpp
+++ 2023/Day3/task2.hpp
@@ -4,12 +4,20 @@
 
 #ifndef DAY1_TASK2_HPP
 #define DAY1_TASK2_HPP
-
+#include <string>
+#include <vector>
 
 class task2 {
 public:
     void Tests();
     void Run();
+    std::vector<int> FindFullNumber(std::string cutted, std::string &s, int startposition);
+    struct search
+    {
+        int pos;
+        bool atstart = false;
+        bool atend = false;
+    };
 };
 
 
